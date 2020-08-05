7aaaca866a2aa734928f6fdb748d2363480dbd2b
jemnon <davidjosepharias@gmail.com>
2020-08-04 19:06:15 -0700

    added another function

--- src/library.test.ts
+++ src/library.test.ts
@@ -1,13 +1,18 @@
-import { getAgeByName, isOddOrEven } from './library';
+import { findMax, getAgeByName, isOddOrEven } from './library';
 
 test('Number is Odd or Even', () => {
   expect(isOddOrEven(2)).toEqual('Even');
-  console.log('log: ', isOddOrEven(2));
+  console.log('isOddOrEven, output: ', isOddOrEven(2));
   expect(isOddOrEven(11)).toEqual('Odd');
-  console.log('log: ', isOddOrEven(11));
+  console.log('isOddOrEven, output: ', isOddOrEven(11));
 });
 
 test('Get age of person by name', () => {
   expect(getAgeByName('Ron')).toEqual('12');
-  console.log('log: ', getAgeByName('Ron'));
+  console.log('getAgeByName, output: ', getAgeByName('Ron'));
+});
+
+test('Find the max number', () => {
+  expect(findMax()).toEqual(5);
+  console.log('findMax, output: ', findMax());
 });
--- src/library.ts
+++ src/library.ts
@@ -1,12 +1,34 @@
+/**
+ * Find the largest item in an unsorted array.
+ * Time Complexity: 3(n) + 2
+ * Big O Notation: O(n)
+ * @param list
+ */
+
+const findMax = (list: number[] = [5, 3, 4, 2]): Number => {
+  let max = null;
+  let counter = 0;
+  list.forEach((item, idx) => {
+    counter++;
+    if (max === null || max < list[idx]) {
+      max = list[idx];
+    }
+  });
+  console.log('counter: ', counter);
+  return max;
+};
+
 interface People {
   [key: string]: string;
 }
+
 const people: People = {
   Ron: '12',
   Bill: '34',
   Horace: '42',
   Roger: '55',
 };
+
 /**
  * `Look-up table` to get age by name. Constant Time - 0(1)
  * @param {String} name
@@ -19,10 +41,10 @@ const getAgeByName = (name: string): string => {
 /**
  * Odd Or Even. Constant Time - O(1)
  * @param {Number} n is input
- * @return {String}
+ * @return {String} Odd or Even
  */
 const isOddOrEven = (n: number): string => {
   return n % 2 ? 'Odd' : 'Even';
 };
 
-export { getAgeByName, isOddOrEven };
+export { findMax, getAgeByName, isOddOrEven };
